3aa000f775221b1dfa136509cd78e2cf50f3bd5d
frio anbia <frioanbia63@gmail.com>
2023-09-11 09:22:51 +0700

    Initial Commit

---
+++ EggsTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EggsTime
+//
+//  Created by Frio Anbia on 11/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
