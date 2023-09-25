4471e9485e0fa6bbcbcc9725a0aecf86db189e41
frio anbia <frioanbia63@gmail.com>
2023-09-25 10:04:47 +0700

    add progresbar in title label DONE! if click soft, medium, hard

--- EggsTime/ViewController.swift
+++ EggsTime/ViewController.swift
@@ -10,36 +10,31 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var secondTimerLabel: UILabel!
-    
     @IBOutlet weak var titleLabel: UILabel!
-    
-    var secondRemaining = 60
-    
+    @IBOutlet weak var progresBarView: UIProgressView!
     var timer = Timer()
-    
-    var eggsTime = [
-        "Soft": 30,
-        "Medium": 45,
-        "Hard": 60
-    ]
+    var eggsTime = ["Soft": 3, "Medium": 5, "Hard": 7]
+    var totalTime = 0
+    var secondsPassed = 0
     
     @IBAction func HardnessSelected(_ sender: UIButton) {
         
         timer.invalidate()
+        let hardness = sender.currentTitle!
+        totalTime = eggsTime[hardness]!
         
-        let hardness = sender.currentTitle! // Soft, Medium, Hard
+        progresBarView.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
         
-        secondRemaining = eggsTime[hardness]!
-        
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         
     }
     
-    @objc func update(){
-        if secondRemaining > 0 {
-            print("\(secondRemaining) seconds.")
-            secondRemaining -= 1
-            secondTimerLabel.text = String("\(secondRemaining) seconds.")
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progresBarView.progress = Float(secondsPassed) / Float(totalTime)
         } else {
             timer.invalidate()
             titleLabel.text = "DONE!"
